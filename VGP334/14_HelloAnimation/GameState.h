4aff43028dc154dc934694c11aa4acb354832b52
Morgan <146908044+MorganChinTang@users.noreply.github.com>
2026-01-07 22:15:59 -0800

    Add 14_HelloAnimation
    Create Framework for animations key frames. Allows transformations of position, rotation, and scale.

---
+++ VGP334/14_HelloAnimation/GameState.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <3DEngine/Inc/3DEngine.h>
+
+class GameState : public Engine3D::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float deltaTime) override;
+	void Render() override;
+	void DebugUI() override;
+
+private:
+
+	void UpdateCamera(float deltaTime);
+
+	Engine3D::Graphics::Camera mCamera;
+	Engine3D::Graphics::DirectionalLight mDirectionalLight;
+
+	Engine3D::Graphics::RenderObject mRenderObject;
+	Engine3D::Graphics::RenderObject mRenderObjectFloor;
+	Engine3D::Graphics::RenderObject mRenderObjectRock;
+	Engine3D::Graphics::StandardEffect mStandardEffect;
+
+	Engine3D::Graphics::Animation mAnimation;
+	float mAnimationTime = 0.0f;
+};
