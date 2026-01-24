79af940ad425f10c03b9208b8f60be8a0112aa24
Morgan <146908044+MorganChinTang@users.noreply.github.com>
2026-01-23 21:37:51 -0800

    Comment out other 2 characters

--- VGP334/16_HelloSkeletonAnimation/GameState.cpp
+++ VGP334/16_HelloSkeletonAnimation/GameState.cpp
@@ -21,11 +21,11 @@ void GameState::Initialize()
 
     mAnimator.Initialize(mCharacter1.modelId);
 
-    mCharacter2.Initialize(L"../../Assets/Models/Character02/Character02.model");
-    mCharacter3.Initialize(L"../../Assets/Models/Character03/Character03.model");
+    //mCharacter2.Initialize(L"../../Assets/Models/Character02/Character02.model");
+    //mCharacter3.Initialize(L"../../Assets/Models/Character03/Character03.model");
 
-    mCharacter2.transform.position = { 2.0f, 0.0f, 0.0f };
-    mCharacter3.transform.position = { -2.0f, 0.0f, 0.0f };
+    //mCharacter2.transform.position = { 2.0f, 0.0f, 0.0f };
+    //mCharacter3.transform.position = { -2.0f, 0.0f, 0.0f };
 
     std::filesystem::path shaderFile = L"../../Assets/Shaders/Standard.fx";
     mStandardEffect.Initialize(shaderFile);
@@ -36,8 +36,8 @@ void GameState::Initialize()
 void GameState::Terminate()
 {
     mCharacter1.Terminate();
-    mCharacter2.Terminate();
-    mCharacter3.Terminate();
+    //mCharacter2.Terminate();
+    //mCharacter3.Terminate();
     mStandardEffect.Terminate();
 }
 void GameState::Update(float deltaTime)
@@ -52,11 +52,11 @@ void GameState::Render()
     {
         AnimationUtil::BoneTransforms boneTransforms;
         AnimationUtil::ComputeBoneTransforms(mCharacter1.modelId, boneTransforms, &mAnimator);
-        AnimationUtil::ComputeBoneTransforms(mCharacter2.modelId, boneTransforms);
-        AnimationUtil::ComputeBoneTransforms(mCharacter3.modelId, boneTransforms);
+        //AnimationUtil::ComputeBoneTransforms(mCharacter2.modelId, boneTransforms);
+        //AnimationUtil::ComputeBoneTransforms(mCharacter3.modelId, boneTransforms);
         AnimationUtil::DrawSkeleton(mCharacter1.modelId, boneTransforms);
-        AnimationUtil::DrawSkeleton(mCharacter2.modelId, boneTransforms);
-        AnimationUtil::DrawSkeleton(mCharacter3.modelId, boneTransforms);
+        //AnimationUtil::DrawSkeleton(mCharacter2.modelId, boneTransforms);
+        //AnimationUtil::DrawSkeleton(mCharacter3.modelId, boneTransforms);
         SimpleDraw::AddGroundPlane(20.0f, Colors::DarkGray);
         SimpleDraw::Render(mCamera);
         
@@ -68,8 +68,8 @@ void GameState::Render()
 
         mStandardEffect.Begin();
         mStandardEffect.Render(mCharacter1);
-        mStandardEffect.Render(mCharacter2);
-        mStandardEffect.Render(mCharacter3);
+        //mStandardEffect.Render(mCharacter2);
+        //mStandardEffect.Render(mCharacter3);
         mStandardEffect.End();
     }
    
