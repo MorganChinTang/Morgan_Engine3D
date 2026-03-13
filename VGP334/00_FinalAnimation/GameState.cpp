fa1ff64afea5342c66a60f68474e89fc16d10990
Morgan <146908044+MorganChinTang@users.noreply.github.com>
2026-03-12 18:39:37 -0700

    added 2 characters for my final
    created 00_FinalAnimation in VGP334

---
+++ VGP334/00_FinalAnimation/GameState.cpp
@@ -0,0 +1,170 @@
+#include "GameState.h"
+using namespace Engine3D;
+using namespace Engine3D::Graphics;
+using namespace Engine3D::Input;
+
+void GameState::Initialize()
+{
+    mCamera.SetPosition({ 0.0f, 1.0f, -3.0f });
+    mCamera.SetLookAt({ 0.0f, 0.0f, 0.0f });
+
+    mDirectionalLight.direction = Math::Normalize({ 1.0f,-1.0f,1.0 });
+    mDirectionalLight.ambient = { 0.4f,0.4f,0.4f,1.0f };
+    mDirectionalLight.diffuse = { 0.7f,0.7f,0.7f,1.0f };
+    mDirectionalLight.specular = { 0.9f,0.9f,0.9f,1.0f };
+
+    mCharacter4.Initialize(L"../../Assets/Models/Character04/Character04.model");
+    mCharacter5.Initialize(L"../../Assets/Models/Character05/Character05.model");
+    mCharacter5.transform.position = { 1.0f, 0.0f, 0.0f };
+    ModelManager* mm = ModelManager::Get();
+    mCharacter4.Initialize("Character04/Character04.model");
+    mCharacter4.Initialize("Character05/Character05.model");
+
+    mCharacter4.animator = &mAnimator4;
+    mCharacter5.animator = &mAnimator5;
+
+    mm->AddAnimation(mCharacter4.modelId, L"../../Assets/Animations/Character04/Talking04.animset");
+    mm->AddAnimation(mCharacter5.modelId, L"../../Assets/Animations/Character05/Talking05.animset");
+
+    mAnimator4.Initialize(mCharacter4.modelId);
+    mAnimator5.Initialize(mCharacter5.modelId);
+
+    std::filesystem::path shaderFile = L"../../Assets/Shaders/Standard.fx";
+    mStandardEffect.Initialize(shaderFile);
+    mStandardEffect.SetCamera(mCamera);
+    mStandardEffect.SetDirectionalLight(mDirectionalLight);
+
+}
+void GameState::Terminate()
+{
+    mCharacter4.Terminate();
+    mCharacter5.Terminate();
+    mStandardEffect.Terminate();
+}
+void GameState::Update(float deltaTime)
+{
+    UpdateCamera(deltaTime);
+    mAnimator4.Update(deltaTime * mAnimationSpeed);
+    mAnimator5.Update(deltaTime * mAnimationSpeed);
+}
+
+void GameState::Render()
+{
+    if (mDrawSkeleton)
+    {
+        AnimationUtil::BoneTransforms boneTransforms;
+        AnimationUtil::ComputeBoneTransforms(mCharacter4.modelId, boneTransforms, &mAnimator4);
+        //AnimationUtil::ComputeBoneTransforms(mCharacter2.modelId, boneTransforms);
+        //AnimationUtil::ComputeBoneTransforms(mCharacter3.modelId, boneTransforms);
+        AnimationUtil::DrawSkeleton(mCharacter4.modelId, boneTransforms);
+        //AnimationUtil::DrawSkeleton(mCharacter2.modelId, boneTransforms);
+        //AnimationUtil::DrawSkeleton(mCharacter3.modelId, boneTransforms);
+        SimpleDraw::AddGroundPlane(20.0f, Colors::DarkGray);
+        SimpleDraw::Render(mCamera);
+        
+    }
+    else
+    {
+        SimpleDraw::AddGroundPlane(20.0f, Colors::DarkGray);
+        SimpleDraw::Render(mCamera);
+
+        mStandardEffect.Begin();
+        mStandardEffect.Render(mCharacter4);
+        //mStandardEffect.Render(mCharacter2);
+        //mStandardEffect.Render(mCharacter3);
+        mStandardEffect.End();
+    }
+   
+}
+
+void GameState::DebugUI()
+{
+    ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+    if (ImGui::CollapsingHeader("Light", ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        if (ImGui::DragFloat3("DirectionalLight", &mDirectionalLight.direction.x, 0.01f))
+        {
+            mDirectionalLight.direction = Math::Normalize(mDirectionalLight.direction);
+        }
+
+        ImGui::ColorEdit4("Ambient#Light", &mDirectionalLight.ambient.r);
+        ImGui::ColorEdit4("Diffuse#Light", &mDirectionalLight.diffuse.r);
+        ImGui::ColorEdit4("Specular#Light", &mDirectionalLight.specular.r);
+    }
+
+    if (ImGui::CollapsingHeader("Material", ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        for (uint32_t i = 0; i < mCharacter4.renderObjects.size(); i++)
+        {
+            Material& material = mCharacter4.renderObjects[i].material;
+            std::string renderObjectId = "RenderObject" + std::to_string(i);
+            ImGui::PushID(renderObjectId.c_str());
+            if (ImGui::CollapsingHeader("RenderObject"))
+            {
+                ImGui::LabelText("Label", "Material: ");
+                ImGui::ColorEdit4("Emissive#Material", &material.emissive.r);
+                ImGui::ColorEdit4("Ambient#Material", &material.ambient.r);
+                ImGui::ColorEdit4("Diffuse#Material", &material.diffuse.r);
+                ImGui::ColorEdit4("Specular#Material", &material.specular.r);
+                ImGui::DragFloat("Shininess#Material", &material.shininess, 0.01f, 0.01f, 1000.0f);
+            }
+            ImGui::PopID();
+        }
+    }
+
+    ImGui::Checkbox("DrawSkeleton", &mDrawSkeleton);
+    ImGui::DragFloat("AnimSpeed", &mAnimationSpeed, 0.01f, 0.0f, 10.0f);
+    int maxAnimations4 = mAnimator4.GetAnimationCount();
+    if (ImGui::DragInt("AnimIndex", &mClipIndex, 1, -1, maxAnimations4 - 1))
+    {
+        mAnimator4.PlayAnimation(mClipIndex, true);
+    }
+    int maxAnimations5 = mAnimator5.GetAnimationCount();
+    if (ImGui::DragInt("AnimIndex", &mClipIndex, 1, -1, maxAnimations5 - 1))
+    {
+        mAnimator5.PlayAnimation(mClipIndex, true);
+    }
+
+
+    mStandardEffect.DebugUI();
+    ImGui::End();
+}
+
+void GameState::UpdateCamera(float deltaTime)
+{
+    InputSystem* input = InputSystem::Get();
+    const float moveSpeed = input->IsKeyDown(KeyCode::LSHIFT) ? 10.0f : 1.0f;
+    const float turnSpeed = 0.1f;
+    if (input->IsKeyDown(KeyCode::W))
+    {
+        mCamera.Walk(moveSpeed * deltaTime);
+    }
+    if (input->IsKeyDown(KeyCode::S))
+    {
+        mCamera.Walk(-moveSpeed * deltaTime);
+    }
+    if (input->IsKeyDown(KeyCode::D))
+    {
+        mCamera.Strafe(moveSpeed * deltaTime);
+    }
+    if (input->IsKeyDown(KeyCode::A))
+    {
+        mCamera.Strafe(-moveSpeed * deltaTime);
+    }
+    if (input->IsKeyDown(KeyCode::Q))
+    {
+        mCamera.Rise(-moveSpeed * deltaTime);
+    }
+    if (input->IsKeyDown(KeyCode::E))
+    {
+        mCamera.Rise(moveSpeed * deltaTime);
+    }
+
+    if (input->IsMouseDown(MouseButton::RBUTTON))
+    {
+        mCamera.Yaw(input->GetMouseMoveX() * turnSpeed * deltaTime);
+        mCamera.Pitch(input->GetMouseMoveY() * turnSpeed * deltaTime);
+    }
+
+}
+
