b5b3cf19a1ba0665cdc60ede306b3950149b50ad
Morgan <146908044+MorganChinTang@users.noreply.github.com>
2026-06-14 00:57:46 -0700

    Final Assignment
    00_HelloGridPlacementFinals is my Final project for VGP336

---
+++ VGP336/00_HelloGridPlacementFinals/GameState.cpp
@@ -0,0 +1,136 @@
+#include "GameState.h"
+#include "math.h"
+
+#include "CustomDebugDrawComponent.h"
+#include "CustomDebugDrawService.h"
+
+using namespace Engine3D;
+using namespace Engine3D::Graphics;
+using namespace Engine3D::Input;
+using namespace Engine3D::Math;
+using namespace Engine3D::Physics;
+
+namespace
+{
+    void SpawnFurniture(GameWorld& gameWorld, GridPlacementController& gridPlacementController)
+    {
+        auto SpawnAt = [&gameWorld, &gridPlacementController](const char* name, const wchar_t* templatePath, const Vector3& position)
+            {
+                GameObject* go = gameWorld.CreateGameObject(name, templatePath);
+                if (go == nullptr)
+                {
+                    return;
+                }
+
+                if (TransformComponent* transform = go->GetComponent<TransformComponent>())
+                {
+                    transform->position = position;
+                }
+
+                go->Initialize();
+                gridPlacementController.RegisterPickableFurniture(*go);
+            };
+
+        SpawnAt(
+            "Furniture_Cactus_01", L"../../Assets/Templates/Objects/furniture_cactus.json",
+            { -3.0f, 0.0f, 2.0f });
+        SpawnAt(
+            "Furniture_Cactus_02", L"../../Assets/Templates/Objects/furniture_cactus.json",
+            { -1.0f, 0.0f, 2.0f });
+        SpawnAt(
+            "Furniture_Salad_01", L"../../Assets/Templates/Objects/furniture_salad.json",
+            { 1.0f, 0.0f, 2.0f });
+        SpawnAt(
+            "Furniture_Salad_02", L"../../Assets/Templates/Objects/furniture_salad.json",
+            { 3.0f, 0.0f, 2.0f });
+    }
+}
+
+Service* MakeCustomService(const std::string& serviceName, GameWorld& gameWorld)
+{
+    if (serviceName == "CustomDebugDrawService")
+    {
+        return gameWorld.AddService<CustomDebugDrawService>();
+    }
+
+    // add another 
+    // add next..
+    return nullptr;
+
+}
+
+Component* MakeCustomComponent(const std::string& componentName, GameObject& gameObject)
+{
+    if (componentName == "CustomDebugDrawComponent")
+    {
+        return gameObject.AddComponent<CustomDebugDrawComponent>();
+    }
+    return nullptr;
+}
+Component* GetCustomComponent(const std::string& componentName, GameObject& gameObject)
+{
+    if (componentName == "CustomDebugDrawComponent")
+    {
+        return gameObject.GetComponent<CustomDebugDrawComponent>();
+    }
+    return nullptr;
+}
+void GameState::Initialize()
+{
+    mLevelFile = L"../../Assets/Templates/Levels/grid_placement_level.json";
+
+    // set a callback to try make a custom service (any serice that is NOT part of the engine and unique to the project)
+    GameWorld::SetCustomService(MakeCustomService);
+    //sets callbacks to try make/get a custom component (any component that is NOT part of the engine)
+    GameObjectFactory::SetCustomMake(MakeCustomComponent);
+    GameObjectFactory::SetCustomGet(GetCustomComponent);
+
+
+    mGameWorld.LoadLevel(mLevelFile);
+    mGridPlacementController.Initialize(mGameWorld);
+    if (PhysicsService* physicsService = mGameWorld.GetService<PhysicsService>())
+    {
+        physicsService->SetEnabled(false);
+    }
+    SpawnFurniture(mGameWorld, mGridPlacementController);
+
+}
+
+void GameState::Terminate()
+{
+    mGridPlacementController.Terminate();
+    mGameWorld.Terminate();
+}
+
+void GameState::Update(float deltaTime)
+{
+    mGameWorld.Update(deltaTime);
+    mGridPlacementController.Update(deltaTime);
+}
+
+void GameState::Render()
+{
+    mGameWorld.Render();
+}
+
+void GameState::DebugUI()
+{
+    ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+    mGridPlacementController.DebugUI();
+    mGameWorld.DebugUI();
+
+    if (ImGui::Button("ReloadLevel"))
+    {
+        mGridPlacementController.Terminate();
+        mGameWorld.Terminate();
+        mGameWorld.LoadLevel(mLevelFile);
+        mGridPlacementController.Initialize(mGameWorld);
+        if (PhysicsService* physicsService = mGameWorld.GetService<PhysicsService>())
+        {
+            physicsService->SetEnabled(false);
+        }
+        SpawnFurniture(mGameWorld, mGridPlacementController);
+    }
+
+    ImGui::End();
+}
---
+++ VGP336/00_HelloGridPlacementFinals/GameState.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "GridPlacementController.h"
+#include <3DEngine/Inc/3DEngine.h>
+
+class GameState : public Engine3D::AppState
+{
+public:
+    void Initialize() override;
+    void Terminate() override;
+    void Update(float deltaTime) override;
+    void Render() override;
+    void DebugUI() override;
+
+private:
+    std::filesystem::path mLevelFile;
+    Engine3D::GameWorld mGameWorld;
+    GridPlacementController mGridPlacementController;
+
+};
